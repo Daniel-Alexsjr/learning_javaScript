f6992909bedd8de77a8e24820fcee29c1ed31dc6
Daniel Alexandre <danielalexandresjr@gmail.com>
2024-12-17 09:53:06 -0300

    DOM for beginners

--- Dom_beginners/animacaoScroll/script.js
+++ Dom_beginners/animacaoScroll/script.js
@@ -72,3 +72,19 @@ initScrollSuave()
 
 
 const sections = document.querySelectorAll('.js-scroll')
+const windowMetade = window.innerHeight * 0.6
+
+function animaScroll(){
+    sections.forEach((section)=>{
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisible = (sectionTop - windowMetade) < 0
+
+        if(isSectionVisible){
+            section.classList.add('ativo')
+        }
+    })
+}
+
+animaScroll()
+
+window.addEventListener('scroll', animaScroll)
